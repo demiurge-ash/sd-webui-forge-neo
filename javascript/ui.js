24f6579c33a11f7981866be28dc9775194883c67
demiurge-ash <demiurge@yandex.ru>
2026-04-13 16:45:39 +0300

    Revert "detect"
    This reverts commit f24db4b7d405e6a1d3dd32f54f384f86c7f40369.

--- javascript/ui.js
+++ javascript/ui.js
@@ -476,10 +476,9 @@ function currentImg2imgSourceResolution(w, h, r) {
     let img = gradioApp().querySelector(
         '#mode_img2img > div[style="display: block;"] :is(img, canvas)',
     );
-    if (!img) return [0, 0, r];
-    const width = img.naturalWidth || img.width;
-    const height = img.naturalHeight || img.height;
-    return [width, height, r];
+    return img
+        ? [img.naturalWidth || img.width, img.naturalHeight || img.height, r]
+        : [0, 0, r];
 }
 
 function updateImg2imgResizeToTextAfterChangingImage() {
