f2c08161a165748ce00e0d5278a2ce32ed1870c8
demiurge-ash <demiurge@yandex.ru>
2026-04-23 19:48:57 +0300

    change default presets

--- modules_forge/presets.py
+++ modules_forge/presets.py
@@ -19,7 +19,7 @@ class PresetArch(Enum):
 
 SAMPLERS = {
     PresetArch.sd: "Euler a",
-    PresetArch.xl: "Euler a",
+    PresetArch.xl: "Restart",
     PresetArch.flux: "Euler",
     PresetArch.klein: "Euler",
     PresetArch.qwen: "LCM",
@@ -31,9 +31,9 @@ SAMPLERS = {
 
 SCHEDULERS = {
     PresetArch.sd: "Automatic",
-    PresetArch.xl: "Automatic",
-    PresetArch.flux: "Beta",
-    PresetArch.klein: "Beta",
+    PresetArch.xl: "Align Your Steps",
+    PresetArch.flux: "FlowMatchEulerDiscrete",
+    PresetArch.klein: "FlowMatchEulerDiscrete",
     PresetArch.qwen: "Normal",
     PresetArch.lumina: "Simple",
     PresetArch.zit: "Beta",
